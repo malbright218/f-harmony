828cf89359745cad685e351a1c7b512c2f004009
Mark Albright <malbright218@gmail.com>
2018-08-06 21:42:41 -0400

    ready to push to github

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+// Data structure that will store the friends data
+var friends = [
+	{
+		name: 'Hermione Granger',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857',
+		scores: [5, 5, 4, 5, 4, 2, 3, 3, 3]
+	},
+	{
+		name: 'Harry Potter',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/a/a9/Dhharryroomhighreso.png/revision/latest?cb=20180510170159',
+		scores: [5, 1, 5, 3, 2, 2, 1, 4, 1]
+	},
+	{
+		name: 'Ginny Weasley',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/8/8b/Ginny_Weasley_hbp_promo.jpg/revision/latest?cb=20180322181904',
+		scores: [1, 5, 4, 3, 1, 3, 2, 3, 2]
+	},
+	{
+		name: 'Ron Weasley',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/a/ab/0143ootp_ron.jpg/revision/latest?cb=20140702164633',
+		scores: [3, 4, 4, 1, 2, 4, 5, 5, 1]
+	},
+	{
+		name: 'Luna Lovegood',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/5/5e/Lunanewhbp.jpg/revision/latest?cb=20100925141354',
+		scores: [1, 1, 1, 1, 4, 1, 5, 5, 1]
+	},
+	{
+		name: 'Draco Malfoy',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/7a/Draco-Malfoy-promo-draco-and-slytherin-22383964-1919-2560.jpg/revision/latest?cb=20140623200502',
+		scores: [1, 3, 2, 4, 2, 4, 2, 4, 5]
+	},
+	{
+		name: 'Fleur Delacour',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/7d/FLEUR1.jpg/revision/latest?cb=20161119222230',
+		scores: [4, 4, 2, 1, 3, 4, 5, 3, 5]
+	},
+	{
+		name: 'Viktor Krum',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/7/71/Viktorkrumyulleball.png/revision/latest?cb=20120207173113',
+		scores: [2, 3, 5, 3, 3, 2, 5, 2, 3]
+	},
+	{
+		name: 'Oliver Wood',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2f/Oliver_WoodDH2.jpg/revision/latest?cb=20110801072255',
+		scores: [2, 3, 3, 3, 4, 1, 3, 3, 5]
+	},
+	{
+		name: 'Newt Scamander',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/4/43/NewtonScamander-Profile-crop.png/revision/latest?cb=20170112223036',
+		scores: [1, 3, 4, 4, 2, 4, 5, 1, 3]
+	}
+]
+
+
+
+
+module.exports = friends;
